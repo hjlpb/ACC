d012dbe4fa2f90fda2c4f7e7fdc1cfaeb4755311
hjlpb <451509741@qq.com>
2020-02-16 18:50:15 +0800

    public to internal
    change the sigVerify and policyCheck from public to internal

--- accessControl.sol
+++ accessControl.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.13;
 
-contract ACC {
+contract ACC {  
     address public owner;
 
     struct Policy {
@@ -12,13 +12,13 @@ contract ACC {
     mapping(address => uint256) clientNonce;
 
     event resultEvent(uint256 resourceID, address clientAddress, string result);
-
+    
     constructor() public {
         owner = msg.sender;
     }
 
     function accessControl(uint256 resourceID, uint256[] memory clientAttributes, uint256 nonce, uint8 v, bytes32 r, bytes32 s) public {
-        //should exist policy for that resourceID
+        //policy should exist for the resourceID
         require(resourcePolicies[resourceID].policyAttributes.length!=0);
         //whether client is revoked
         require(clientNonce[msg.sender] == nonce);
@@ -29,8 +29,9 @@ contract ACC {
         //triger event
         emit resultEvent(resourceID, msg.sender, "allow");
     }
+    
 
-    function sigVerify(address clientAddress, uint256[] memory clientAttributes, uint256 nonce, uint8 v, bytes32 r, bytes32 s) public view returns(bool) {
+    function sigVerify(address clientAddress, uint256[] memory clientAttributes, uint256 nonce, uint8 v, bytes32 r, bytes32 s) internal view returns(bool) {
         bytes32 messageHash = keccak256(abi.encodePacked(clientAddress, clientAttributes, nonce));
         bytes32 prefixedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
         if (ecrecover(prefixedMessageHash, v, r, s) == owner){
@@ -40,8 +41,8 @@ contract ACC {
             return false;
         }
     }
-
-    function policyCheck(uint256 resourceID, uint256[] memory clientAttributes) public view returns(bool) {
+    
+    function policyCheck(uint256 resourceID, uint256[] memory clientAttributes) internal view returns(bool) {
         Policy memory policy = resourcePolicies[resourceID];
         uint256[] memory policyAttributes = policy.policyAttributes;
         uint256 matchingAttributes;
@@ -60,7 +61,7 @@ contract ACC {
             return false;
         }
     }
-
+    
     function addResPoly(uint256 resourceID, uint256 k, uint256[] memory policyAttributes) public {
         require(msg.sender == owner);
         Policy memory resourcePolicy = Policy({
